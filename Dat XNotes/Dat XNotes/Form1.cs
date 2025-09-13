18f4dc96295d76afe346e32697ec243af76efb0c
Dauod465 <dauod.ali2010@gmail.com>
2025-09-13 10:22:55 +0300

    Add files via upload

---
+++ Dat XNotes/Dat XNotes/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
+
+namespace Dat_XNotes
+{
+    public partial class XNotes : Form
+    {
+        public XNotes()
+        {
+            InitializeComponent();
+        }
+
+        private void cToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog Openfile = new OpenFileDialog();
+            Openfile.Title = "Open";
+            Openfile.Filter = "Text Document(*.txt)|*.txt|All Files(*.*)|*.*";
+            if (Openfile.ShowDialog() == DialogResult.OK)
+                richTextBox1.LoadFile(Openfile.FileName, RichTextBoxStreamType.PlainText);
+            this.Text = Openfile.FileName;
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog Savefile = new SaveFileDialog();
+            Savefile.Title = "Save";
+            Savefile.Filter = "Text Document(*.txt)|*.txt|All Files(*.*)|*.*";
+            if (Savefile.ShowDialog() == DialogResult.OK)
+                richTextBox1.SaveFile(Savefile.FileName, RichTextBoxStreamType.PlainText);
+            this.Text = Savefile.FileName;
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void dateTimeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = System.DateTime.Now.ToString();
+        }
+
+        private void undoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Undo();
+        }
+
+        private void redoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Redo();
+        }
+
+        private void cutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Cut();
+        }
+
+        private void copyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Copy();
+        }
+
+        private void pasteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Paste();
+        }
+
+        private void selectAllToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.SelectAll();
+        }
+
+        private void fontToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FontDialog fnt = new FontDialog();
+            if(fnt.ShowDialog() == DialogResult.OK)
+                richTextBox1.Font = fnt.Font;
+        }
+
+        private void colorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ColorDialog TextColor = new ColorDialog();
+            if(TextColor.ShowDialog() == DialogResult.OK)
+                richTextBox1.ForeColor = TextColor.Color;
+        }
+    }
+}
